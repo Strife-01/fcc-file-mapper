1de5b18effc70ee1daebe5ecf39cd9ab9f8e8e7c
Andrei <andreiursachi10@yahoo.com>
2023-05-19 19:49:46 +0300

    fix: added the entire project

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    const file = req.file;
+    res.json({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.buffer.length,
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port);
 });
